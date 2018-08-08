7fb96ddd8989bbe10a0779bb15d302b2a8b69bcb
Tony Geiser <geisera@gmail.com>
2018-08-08 11:30:15 -0500

    p

--- index.php
+++ index.php
@@ -9,7 +9,17 @@
 				<?php
 
 				echo "Php test succesful!";
-
+				$x = "This is a string variable.";
+				$y = 41;
+				$z = 6;
+				$t = $y;
+				$v = $y * $z;
+				
+				echo $x;
+				echo $y;
+				echo $z;
+				echo $t;
+				echo $v;
 				?>
 			</h1>
 		</center>
